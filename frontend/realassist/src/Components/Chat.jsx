d7c969bf11e190689d9ee9e8e8ffedd0a0b08718
ptlharshil <patelharshil3695@gmail.com>
2023-02-14 00:52:18 -0500

    frontend working

--- frontend/realassist/src/Components/Chat.jsx
+++ frontend/realassist/src/Components/Chat.jsx
@@ -1,61 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import "./Chat.css"
-const Chat = ({chatDetails, setChatDetails, addChat,newTitle, num, setI, setAddChat, addTime, setAddTime, showChat,setShowChat}) => {
-    const [chatMsg,setChatMsg]=useState("")
+const Chat = ({ chatDetails, setChatDetails, chatIndex, addChat, newTitle, num, setI, setAddChat, addTime, setAddTime, showChat, setShowChat }) => {
+    const [chatMsg, setChatMsg] = useState("")
+    const [chatState, setChatState] = useState(chatDetails[chatIndex]);
+    const { messages, time } = chatState
 
-    // useEffect(()=>{
-    //     setChatDetails(...chatDetails)
-    //     console.log(chatDetails[num].content.messages)
-    // })
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
         setChatMsg(e.target.value)
     }
-    const chatMsgs=(e)=>{
-        if(e.key==="Enter")
-        {
-            const time=new Date()
-            // setAddChat(...addChat,chatMsg)
-            const t2=time.getHours()+ ":" + time.getMinutes()
-            // setAddTime(...addTime,t2)
-            const chat={
-                title: newTitle,
-                user:"You",
-                content:{
-                    messages:[...addChat,chatMsg],
-                    time: [...addTime,t2]
-                }
+    const chatMsgs = (e) => {
+        if (e.key === "Enter") {
 
-            }
-            setChatDetails([chat])
-            setChatMsg("")
-            console.log(chatDetails)
+            const t2 = new Date().getHours() + ":" + new Date().getMinutes();
+            const newMessages = [...messages, chatMsg];
+            const newTime = [...time, t2];
+            const newChatState = { ...chatState, messages: newMessages, time: newTime };
+            setChatState(newChatState);
+
+            const newChatDetails = [...chatDetails];
+            newChatDetails[chatIndex] = newChatState;
+            setChatDetails(newChatDetails);
+
+            setChatMsg("");
         }
+
     }
-  return (
-  <div className='chatwindow'>
-    
-        {chatDetails[num].content.messages.map((chat,i)=>{
-            return(<div className='message'>.
-                <h4 className='msg' key={i}>{chat}</h4>
-                
-            </div>)
-        })}
-        {chatDetails[num].content.time.map((chat,i)=>{
-            return(
-                <h6 className='time'>{chat}</h6>
-                
-            )
-        })}
 
-        { chatDetails.length>0 && 
-            <div >
-                <div className='fields'>
-                    <input type="text" value={chatMsg} className='input' onChange={handleChange} onKeyDown={chatMsgs} />
-                    <button className='audio' >V</button>
+
+    return (
+        <div className='chatwindow'>
+
+            {chatDetails[chatIndex].messages.map((chat, i) => {
+                return (<div className='message'>
+                    <h4 className='msg' key={i}>{chat}</h4>
+
+                </div>)
+
+            })}
+
+            {chatDetails.length > 0 &&
+                <div >
+                    <div className='fields'>
+                        <input type="text" value={chatMsg} className='input' onChange={handleChange} onKeyDown={(e) => chatMsgs(e)} />
+                        <button className='audio' >V</button>
+                    </div>
                 </div>
-            </div>
-        }
-    </div>
+            }
+        </div>
     )
 
 }
--- frontend/realassist/src/Components/SideBar.jsx
+++ frontend/realassist/src/Components/SideBar.jsx
@@ -1,196 +1,180 @@
-import  { useEffect, useState } from 'react'
+import { useState } from 'react'
 import * as React from 'react';
 import Chat from "../Components/Chat"
 import { styled } from '@mui/material/styles';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import NativeSelect from '@mui/material/NativeSelect';
 import InputBase from '@mui/material/InputBase';
 import TextField from '@mui/material/TextField';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 import "./SideBar.css"
-import Titles from "../Components/Titles"
 const SideBar = () => {
-    const [showChat,setShowChat]=useState(false)
-    const [chatDetails,setChatDetails]=useState([])
-    const [addChat, setAddChat]=useState(["Hey, this is Real Assist AI, how can I help you?"])
-    const [addTime, setAddTime]=useState([])
-    var chatTitle=""
-    const [num,setNum]=useState('')
-    const [show, setShow]=useState(false)
-    const [newTitle,setNewTitle]=useState("New Chat")
-    // const [i,setI]=useState('')
-    
-    const BootstrapInput = styled(InputBase)(({ theme }) => ({
-        'label + &': {
-          marginTop: theme.spacing(3),
-        },
-        '& .MuiInputBase-input': {
-          borderRadius: 4,
-          position: 'relative',
-          backgroundColor: theme.palette.background.paper,
-          border: '1px solid #ced4da',
-          fontSize: 16,
-          padding: '10px 26px 10px 12px',
-          transition: theme.transitions.create(['border-color', 'box-shadow']),
-          // Use the system font instead of the default Roboto font.
-          fontFamily: [
-            '-apple-system',
-            'BlinkMacSystemFont',
-            '"Segoe UI"',
-            'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            'sans-serif',
-            '"Apple Color Emoji"',
-            '"Segoe UI Emoji"',
-            '"Segoe UI Symbol"',
-          ].join(','),
-          '&:focus': {
-            borderRadius: 4,
-            borderColor: '#80bdff',
-            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-          },
-        },
-      }));
+  const [showChat, setShowChat] = useState(false)
+  const [chatDetails, setChatDetails] = useState([])
+  const [addChat, setAddChat] = useState(["Hey, this is Real Assist AI, how can I help you?"])
+  const [addTime, setAddTime] = useState([])
+  const [num, setNum] = useState('')
+  const [show, setShow] = useState(false)
+  const [newTitle, setNewTitle] = useState("New Chat")
 
-      
-        const handleChange = (index) => {
-          chatDetails.map((cd,i)=>{
-            if(index===i)
-            {
-                setShow(true)    
-                setNum(i)
-                setShowChat(true)
-            }
-          })
-        };
+  const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+    '& .MuiInputBase-input': {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.background.paper,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      padding: '10px 26px 10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        borderRadius: 4,
+        borderColor: '#80bdff',
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+      },
+    },
+  }));
 
-        const handleNo=()=>{
-            setShow(false)
-            setShowChat(true)
-        }
-        const handleYes=(newTitle,index)=>{
-            chatDetails.map((cd,i)=>{
-                if(index===i)
-                {
-                    cd.title=newTitle
-                }
-              })
-              setChatDetails([...chatDetails])
-              setShow(false) 
-              setShowChat(true)   
 
-        }
-        const handleText=(e)=>{
-            setNewTitle(e.target.value)
-        }
-    const deleteChat=(index)=>{
-          setChatDetails(chatDetails.filter((chat,i)=>index!==i))
-          
+  const handleChange = (index) => {
+    chatDetails.map((cd, i) => {
+      if (index === i) {
+        setShow(true)
+        setNum(i)
+      }
+    })
+  };
+
+  const handleNo = () => {
+    setShow(false)
+    setShowChat(true)
+  }
+  const handleYes = (newTitle, index) => {
+    chatDetails.map((cd, i) => {
+      if (index === i) {
+        cd.title = newTitle
+      }
+    })
+    setChatDetails([...chatDetails])
+    setShow(false)
+    setShowChat(true)
+
+  }
+  const handleText = (e) => {
+    setNewTitle(e.target.value)
+  }
+  const deleteChat = (index) => {
+    setChatDetails(chatDetails.filter((chat, i) => index !== i))
+
+  }
+
+
+  const newChat = () => {
+    const time = new Date()
+    addTime.push(time.getHours() + ":" + time.getMinutes())
+    const chat = {
+      title: "New Chat",
+      user: "AI",
+      messages: addChat,
+      time: addTime
+
     }
-    const handleChatRoom=(index)=>{
-        
-            
-            setNum(index)
-            
-    
-    }
+    { chatDetails.length === 0 && setNum(0) }
+    setChatDetails([...chatDetails, chat])
+    setShowChat(true)
+  }
 
 
-
-
-
-
-
-    
-
-    const newChat=()=>{
-        const time=new Date()
-        addTime.push(time.getHours() + ":" + time.getMinutes())
-        const chat={
-            title:"New Chat",
-            user:"You",
-            content:{
-                messages:addChat,
-                time: addTime
-            }
-        }
-        {chatDetails.length===0 && setNum(0)}
-        // chatDetails.push(chat)
-        setChatDetails([...chatDetails,chat])
-        setShowChat(true)
-        console.log(chatDetails)
-    }
-
-    
-  return (<>
-    <div className='sidebar'>
+  return (
+    <>
+      <div className='sidebar'>
         <div className='heading'>
-            <h2 className='name'>Recent Chats</h2>
-            <button className='create' onClick={newChat}>New Chat</button>
+          <h2 className='name'>Recent Chats</h2>
+          <button className='create' onClick={newChat}>New Chat</button>
         </div>
-        
-        {/* <Titles chatDetails={chatDetails} setChatDetails={setChatDetails} showChat={showChat} 
-         setShowChat={setShowChat} addChat={addChat}
-         setAddChat={setAddChat} addTime={addTime} setAddTime={setAddTime}/> */}
-        
+
+
         <>
-    { chatDetails.map((chats,index)=>
-    
-    <div className='chatnames'>
-       
-        {show && num===index ?<> <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue={chats.title}
-          variant="filled"
-          onChange={handleText}
-          style={{marginRight:"55px", marginTop:"-42px"}}/>
-          <ButtonGroup
-          disableElevation
-          variant="contained"
-          aria-label="Disabled elevation buttons"
-        >
-          <Button onClick={()=>handleYes(newTitle,index)}>Yes</Button>
-          <Button onClick={handleNo}>No</Button>
-        </ButtonGroup></>
-        :<> <h3 key={index} className="title" onClick={()=>handleChatRoom(index)} style={{cursor:"pointer"}}>{chats.title}</h3>
-        <FormControl  style={{margin:"5px 5px 5px 5px"}}>
-        <Select
-          labelId="demo-customized-select-label"
-          id="demo-customized-select"
-          input={<BootstrapInput />}
-          key={index}
-        >
-            
-          <MenuItem value={10} onClick={()=>handleChange(index)}>Edit</MenuItem>
-          <MenuItem value={20} onClick={()=>deleteChat(index)}>Delete</MenuItem>
-        </Select>
-      </FormControl>
-            
-      {showChat && index===num && <Chat chatDetails={chatDetails} newTitle={newTitle} 
-      setChatDetails={setChatDetails} num={num} addChat={addChat} addTime={addTime}
-          />}
-      </>}         
-    </div>)}
+          {chatDetails.map((chats, index) =>
+            <>
+              {showChat && index === num && <Chat chatDetails={chatDetails} newTitle={newTitle}
+                setChatDetails={setChatDetails} num={num} addChat={addChat} addTime={addTime} chatIndex={index}
+              />}
+              <div className='chatnames'>
 
-    
-    
-    
+                {show && num === index ?
+                  <>
+                    <TextField
+                      required
+                      id="filled-required"
+                      label="Required"
+                      defaultValue={chats.title}
+                      variant="filled"
+                      onChange={handleText}
+                      style={{ marginRight: "55px", marginTop: "-42px" }} />
+                    <ButtonGroup
+                      disableElevation
+                      variant="contained"
+                      aria-label="Disabled elevation buttons"
+                    >
+                      <Button onClick={() => handleYes(newTitle, index)}>Yes</Button>
+                      <Button onClick={handleNo}>No</Button>
+                    </ButtonGroup>
 
-   
-    </>
-        
-    </div>
-    {/* {showChat && <Chat chatDetails={chatDetails} setChatDetails={setChatDetails} addChat={addChat} 
-             i={i} setI={setI} setAddChat={setAddChat} addTime={addTime} setAddTime={setAddTime} 
-             showChat={showChat} setShowChat={setShowChat}/>} */}
-   
+
+
+                  </>
+                  :
+                  <>
+                    {chatDetails.map((chat, index) => (<>
+                      <div key={index}>
+                        <h3 className='title' onClick={() => {
+                          setShowChat(true);
+                          setNum(index);
+                        }}>
+                          {chat.title}
+                        </h3>
+                      </div>
+                      <FormControl style={{ margin: "5px 5px 5px 5px" }}>
+                        <Select
+                          labelId="demo-customized-select-label"
+                          id="demo-customized-select"
+                          input={<BootstrapInput />}
+                          key={index}
+                        >
+
+                          <MenuItem value={10} onClick={() => handleChange(index)}>Edit</MenuItem>
+                          <MenuItem value={20} onClick={() => deleteChat(index)}>Delete</MenuItem>
+                        </Select>
+                      </FormControl></>
+                    ))}
+
+                  </>}
+
+
+              </div>
+            </>)}
+        </>
+
+      </div>
+
+
     </>
   )
 }
